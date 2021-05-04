aabcdb62bc7e0baaa341c27f48a45e71d1adfdb9
Daniela Caus <danicaus.br@gmail.com>
2021-05-04 17:17:23 -0300

    Changes slider travel distance

--- scripts/main.js
+++ scripts/main.js
@@ -4,11 +4,11 @@ const slider=document.querySelector('.timeline-slider')
 var x = 0
 
 previousPage.addEventListener('click', function moveBack(){
-    x = x + 100
+    x = x + 265
     slider.style = `transform:translateX(${x}px);`
 })
 
 nextPage.addEventListener('click', function moveForward(){
-    x = x - 100
+    x = x - 265
     slider.style = `transform:translateX(${x}px);`
 })
