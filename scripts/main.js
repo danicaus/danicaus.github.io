ebd6aed22f036a7195bb646f6f26dd1901b38fcc
Daniela Caus <danicaus.br@gmail.com>
2021-07-20 14:09:48 -0300

    Add carousel into portfolio-pages

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,26 @@
-const previousPage=document.querySelector('.-btPrevious')
-const nextPage=document.querySelector('.-btNext')
-const slider=document.querySelector('.timeline-slider')
-var x = 0
+import { Carousel } from "./carousel.js";
 
-previousPage.addEventListener('click', function moveBack(){
-    x = x + 270
-    slider.style = `transform:translateX(${x}px);`
-})
+// const previousPage=document.querySelector('.-btPrevious')
+// const nextPage=document.querySelector('.-btNext')
+// const slider=document.querySelector('.timeline-slider')
+// var x = 0
 
-nextPage.addEventListener('click', function moveForward(){
-    x = x - 270
-    slider.style = `transform:translateX(${x}px);`
-})
+// previousPage.addEventListener('click', function moveBack(){
+//     x = x + 270
+//     slider.style = `transform:translateX(${x}px);`
+// })
+
+// nextPage.addEventListener('click', function moveForward(){
+//     x = x - 270
+//     slider.style = `transform:translateX(${x}px);`
+// })
+
+
+
+const anterior = '[data-anterior]'
+const proximo = '[data-proximo]'
+const listaProdutos = '[data-lista-slides]'
+const navegacao = '[data-navegacao]'
+
+
+new Carousel(anterior, proximo,listaProdutos,navegacao)
